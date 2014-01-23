5d7e418ed75a6df304c5d0bc477d99d111664bd7
aoisensi <aoisensi@engineer.(none)>
2014-01-24 05:03:18 +0900

    clean

--- include/APP.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _APP_H_
-
-#define SAFE_DELETE(x) { delete x; x=NULL; }
-#define SAFE_DELETE_ARRAY(x) { delete[] x; x=NULL }
-
-#endif
--- include/lexer.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __HPP_LEXER
-#define __HPP_LEXER
-
-#include "lexer/tokentype.hpp"
-#include "lexer/token.hpp"
-#include "lexer/tokenstream.hpp"
-#include "lexer/lexicalanalysis.hpp"
-
-#endif
